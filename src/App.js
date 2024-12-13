f988a17fe6621e4a30993699eb5c3d45fc911cfe
Anuska <anuskasth2022@gmail.com>
2024-12-13 22:54:24 +1100

    adding sibling component check

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import PropTest from "./PropTest";
+import Profile from "./Profile";
 
-function App() {
+const App = () => {
+  const name = "Anuska Shrestha";
   return (
     <div>
-      <PropTest />
       <h2>Hello world;</h2>
+      <Profile />
+      <PropTest />
     </div>
   );
-}
+};
 
 export default App;
