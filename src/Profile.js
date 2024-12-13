077fe5fa27cb6b2dc9a0e481e514a3266d3fab4e
Anuska <anuskasth2022@gmail.com>
2024-12-13 23:24:59 +1100

    props revise done

--- src/Profile.js
+++ src/Profile.js
@@ -1,12 +1,16 @@
 import React from "react";
 
-const Profile = (props) => {
+const Profile = ({ nameProps, bio }) => {
+  //   const{ nameProps, bio } = props;
   return (
     <div>
-      HI this is {props.nameProps}. {props.bio}
+      {/* HI this is {props.nameProps}. {props.bio} */}
+      HI this is {nameProps} and {bio}
       <hr />
     </div>
   );
 };
 
 export default Profile;
+
+// destructuring helps to reduce code (line of code : devlopment is accelerated)
